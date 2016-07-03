6b0847b94506a3edfb75a849b09c2f18f9c435f1
Jack Bridger <Jack@Jacks-MacBook-Pro.local>
2016-07-03 22:08:31 +0100

    first day building done.. lists the teachers/students on opposing interface

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,6 +1,8 @@
 class StudentController < ApplicationController
 
     def index
-
+        @teacher = Teacher.where('email' => current_user.teacher_email)
     end
+
 end
+#where('email' => current_user.teacher_email)
--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -1,5 +1,13 @@
 class TeacherController < ApplicationController
 
     def index
+        @all_users = Student.where('teacher_email' => current_user.email)
     end
+#User.where(teacher_email: current_user.email)
+    def first_name
+        @first_name = Teacher.first_name
+    end
+
+
+
 end
