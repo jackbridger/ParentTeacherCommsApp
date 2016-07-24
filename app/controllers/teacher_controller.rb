33ed2fa5aed321a7a8f6094c35f114649993bce8
Jack Bridger <Jack@Jacks-MacBook-Pro.local>
2016-07-24 12:35:28 +0100

    make it better

--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -5,7 +5,7 @@ before_filter :authenticate_user!
         @all_students = Student.where('teacher_email' => current_user.email)
 
         #feedback
-        @all_generated_feedback = GeneratedFeedback.where({teacher_id: current_user.id, subject: 'English'}).all
+        #@all_generated_feedback = GeneratedFeedback.where({teacher_id: current_user.id, subject: 'English'}).all
 
 
         #tasks
