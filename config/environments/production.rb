33bb9a08c5f9d27b87463c587b67d1cdda8c2c19
Jack Bridger <Jack@Jacks-MacBook-Pro.local>
2016-07-24 12:13:47 +0100

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,14 +4,17 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  #adding as suggested
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Disable serving static files from the `/public` folder by default since
