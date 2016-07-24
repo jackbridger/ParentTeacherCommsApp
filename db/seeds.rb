5b75ef7f0a2442aceaacdb1c5d911b85f00daa90
Jack Bridger <Jack@Jacks-MacBook-Pro.local>
2016-07-24 19:38:47 +0100

    added seeds etc.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: 'Sarah', last_name: 'Cornish', email: 'sarah@gmail.com', type: 'Teacher', password: 'hockey')
+User.create(first_name: 'Jack', last_name: 'Bridger', email: 'jack@gmail.com', type: 'Student', password: 'hockey', teacher_email: 'sarah@gmail.com')
