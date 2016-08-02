4155f7a4acf8ad2484f33159b2411a7f20acb293
Jack Bridger <Jack@Jacks-MacBook-Pro.local>
2016-08-02 17:30:05 +0100

    should have changed the production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,20 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+    config.action_mailer.raise_delivery_errors = true
+
+  #change mail delivery to either :smtp, :sendmail, :file, :test ---JB (using railscast)
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => ENV['parentcommsapp@gmail.com'],
+ :password             => ENV['parentcommunication'],
+ :authentication       => "plain",
+:enable_starttls_auto => true
+}
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
