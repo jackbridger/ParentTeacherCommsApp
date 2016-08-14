782465e719ff6445cd9d5116edad8622b20b5413
Jack Bridger <Jack@Jacks-MacBook-Pro.local>
2016-08-14 02:02:30 +0100

    charts working

--- app/helpers/teacher_helper.rb
+++ app/helpers/teacher_helper.rb
@@ -1,2 +1,21 @@
 module TeacherHelper
+
+
+def students_by_logins
+    pie_chart @all_students.group(:sign_in_count).count, height: '300px', width: '300px', library: {
+      title: {text: 'How often parents have logged in', x: -20},
+      yAxis: {
+         allowDecimals: false,
+         title: {
+             text: 'number of parents'
+         }
+      },
+      xAxis: {
+         title: {
+             text: 'number of logins'
+         }
+      }
+    }
+  end
+
 end
