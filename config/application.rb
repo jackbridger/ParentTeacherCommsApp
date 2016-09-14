24832342d4b5db38deeaf9a5a0e0ce9e6d9ff668
Jack Bridger <Jack@Jacks-MacBook-Pro.local>
2016-09-14 11:36:28 +0100

    trying to fix the timezone

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,8 @@ module Sti
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.initialize_on_precompile = false
-    config.time_zone = 'London'
+    config.time_zone = 'GB'
+
   end
 end
 
